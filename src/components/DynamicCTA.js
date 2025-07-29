a2e5b81b8bc40bcf557a24fee0626b887e10990d
Aditto dev Barmon (koushik) <134207015+Adityo-dev@users.noreply.github.com>
2025-07-30 05:30:30 +0600

    All course almost [Done]

---
+++ src/components/DynamicCTA.js
@@ -0,0 +1,16 @@
+function DynamicCTA({ heading, description }) {
+  return (
+    <section className="relative overflow-hidden bg-[#114C5F] px-4 py-32">
+      <div className="relative container sm:max-w-[60%] mx-auto text-center space-y-2 sm:space-y-4 z-30">
+        <h3 className="text-[#FFF] text-[48px] font-semibold leading-[62px]">
+          {heading}
+        </h3>
+        <p className="sm:w-[60%] mx-auto text-lg text-[#D4D4D4] sm:leading-relaxed">
+          {description}
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default DynamicCTA;
